58ad2a7cdc1fa67a5c87b6eb20c0b04695ea6ca1
Aibol Zheksembay <98872952+Aibol12@users.noreply.github.com>
2022-02-05 12:49:52 +0300

    Update i.py

--- lab1/i.py
+++ lab1/i.py
@@ -1,11 +1,8 @@
-a= int(input())
-ans = ""
-for i in range(a):
-    b = (input())
-    if "@" in b:
-      tv = b.split("@")
-      if tv[1] == "gmail.com":
-         ans += tv[0] + " "
-tva = ans.split()
-for s in range(len(tva)):
-    print(tva[s])
+n=int(input())
+a=[]
+for i in range(n):
+   a.append(str(input()))
+for i in a:
+   if i[len(i)-10:len(i)]=="@gmail.com":
+       i=i[0:len(i)-10]
+       print(i)
