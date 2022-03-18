9d419af1c167b80f4b7dfcfb780dd77f5c5e92ae
Aibol Zheksembay <98872952+Aibol12@users.noreply.github.com>
2022-03-18 18:51:05 +0300

    Update d7.py

--- lab6/dir and files/d7.py
+++ lab6/dir and files/d7.py
@@ -1,4 +1,4 @@
-with open('ex.txt', 'r') as first:
-    with open('example.txt', 'w') as second:
+with open('a.txt', 'r') as first:
+    with open('b.txt', 'w') as second:
         for line in first:
             second.writelines(line)
