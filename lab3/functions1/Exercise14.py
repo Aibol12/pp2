616832939a83fd25ee4e85d51212ed42cae034db
Aibol Zheksembay <aibol.zheksembai.02@mail.ru>
2024-06-28 17:14:56 +0600

    pp2

--- lab3/functions1/Exercise14.py
+++ lab3/functions1/Exercise14.py
@@ -1,9 +1,8 @@
-import Exercise1, Exercise2, Exercise12
+import Exercise1, Exercise2
 
 ounces = grams_to_ounces(grams)
 print(ounces)
 celsius = fahrenheit_to_celsius(fahrenheit)
 print( celsius)
-numbers = [int(x) for x in input_numbers.split()]
-histogram(numbers)
+
 
